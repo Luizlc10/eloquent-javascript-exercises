9cd702ca7c5f56b22e3e6b5978ff663f9b0b8c58
Luiz Cláudio <luiz.lclr@gmail.com>
2019-08-05 18:03:41 -0300

    add 'reversing-an-array' exercise - chapter 4

---
+++ 04-data-structures-objects-and-arrays/reversing-an-array.js
@@ -0,0 +1,19 @@
+function reverseArray(array) {
+  let result = [];
+  for (const element of array) result.unshift(element);
+  return result;
+}
+
+function reverseArrayInPlace(array) {
+  for(let i = 0; i < Math.floor(array.length / 2); i++) {
+    let temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
